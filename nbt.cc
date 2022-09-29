19f4693cb72de90701f1b56d78deab4d50d3ed43
0x526f6f74 <110607585+0x526f6f74@users.noreply.github.com>
2022-09-29 18:09:00 +0200

    Add files via upload

---
+++ nbt.cc
@@ -0,0 +1,251 @@
+#include "nbt.h"
+
+namespace nbt
+{
+
+namespace detail
+{
+
+void encode(std::ostream& os, Is_tag_end auto)
+{
+    os.put(TAG_END);
+}
+
+void encode(std::ostream& os, Is_tag_numeric auto value)
+{
+    os.write(reinterpret_cast<char*>(&value), sizeof(value));
+}
+
+void encode(std::ostream& os, const Is_tag_string auto& string)
+{
+    encode<TagShort>(os, string.length());
+    os.write(string.data(), string.length());
+}
+
+void encode(std::ostream& os, const Is_tag_array auto& vector)
+{
+    encode<TagInt>(os, vector.size());
+    for (auto element : vector)
+        encode(os, element);
+}
+
+void encode(std::ostream& os, const Is_tag_or_tag_list auto& value)
+{
+    value.encode(os);
+}
+
+void encode(std::ostream& os, const Is_tag_compound auto& compound)
+{
+    for (const auto& [key, tag] : compound)
+    {
+        encode<TagByte>(os, tag.index());
+        encode(os, key);
+        encode(os, tag);
+    }
+    encode<TagEnd>(os);
+}
+
+template<Is_tag_end T>
+T decode(std::istream& is)
+{
+    is.ignore();
+    return {};
+}
+
+template<Is_tag_numeric T>
+T decode(std::istream& is)
+{
+    T value;
+    is.read(reinterpret_cast<char*>(&value), sizeof(value));
+    return value;
+}
+
+template<Is_tag_string T>
+T decode(std::istream& is)
+{
+    auto len = decode<TagShort>(is);
+    TagString string(len, '\0');
+    is.read(string.data(), len);
+    return string;
+}
+
+template<Is_tag_array T>
+T decode(std::istream& is)
+{
+    T vector(decode<TagInt>(is));
+    for (auto& element : vector)
+        element = decode<typename T::value_type>(is);
+    return vector;
+}
+
+template<Is_tag_list T>
+T decode(std::istream& is)
+{
+    return TagList {is};
+}
+
+template<Is_tag_compound T>
+T decode(std::istream& is)
+{
+    TagCompound compound;
+    for (auto type = decode<TagByte>(is); type != TAG_END; type = decode<TagByte>(is))
+    {
+        auto key = decode<TagString>(is);
+        compound[key] = Tag {is, static_cast<TagType>(type)};
+    }
+    return compound;
+}
+
+}  // namespace detail
+
+TagList::TagList(std::istream& is)
+{
+    this->decode(is);
+}
+
+void TagList::encode(std::ostream& os) const
+{
+    detail::encode<TagByte>(os, this->index());
+    const auto encoder = [&](const auto& vector)
+    {
+        detail::encode<TagInt>(os, vector.size());
+        for (const auto& value : vector)
+            detail::encode(os, value);
+    };
+    std::visit(encoder, *static_cast<const variant*>(this));
+}
+
+void TagList::decode(std::istream& is)
+{
+    switch (detail::decode<TagByte>(is))
+    {
+        case TAG_END: *this = this->decode_vector<TagEnd>(is); break;
+        case TAG_BYTE: *this = this->decode_vector<TagByte>(is); break;
+        case TAG_SHORT: *this = this->decode_vector<TagShort>(is); break;
+        case TAG_INT: *this = this->decode_vector<TagInt>(is); break;
+        case TAG_LONG: *this = this->decode_vector<TagLong>(is); break;
+        case TAG_FLOAT: *this = this->decode_vector<TagFloat>(is); break;
+        case TAG_DOUBLE: *this = this->decode_vector<TagDouble>(is); break;
+        case TAG_BYTE_ARRAY: *this = this->decode_vector<TagByteArray>(is); break;
+        case TAG_STRING: *this = this->decode_vector<TagString>(is); break;
+        case TAG_LIST: *this = this->decode_vector<TagList>(is); break;
+        case TAG_COMPOUND: *this = this->decode_vector<TagCompound>(is); break;
+        case TAG_INT_ARRAY: *this = this->decode_vector<TagIntArray>(is); break;
+        case TAG_LONG_ARRAY: *this = this->decode_vector<TagLongArray>(is); break;
+        default: throw std::runtime_error {"invalid tag type"};
+    }
+}
+
+template<typename T>
+std::vector<T> TagList::decode_vector(std::istream& is) requires detail::Is_tag_type<T>
+{
+    std::vector<T> vector(detail::decode<TagInt>(is));
+    for (auto& element : vector)
+        element = detail::decode<T>(is);
+    return vector;
+}
+
+Tag::Tag(std::istream& is, TagType type)
+{
+    this->decode(is, type);
+}
+
+void Tag::encode(std::ostream& os) const
+{
+    const auto encoder = [&](const auto& value) { detail::encode(os, value); };
+    std::visit(encoder, *static_cast<const variant*>(this));
+}
+
+void Tag::decode(std::istream& is, TagType type)
+{
+    switch (type)
+    {
+        case TAG_END: *this = detail::decode<TagEnd>(is); break;
+        case TAG_BYTE: *this = detail::decode<TagByte>(is); break;
+        case TAG_SHORT: *this = detail::decode<TagShort>(is); break;
+        case TAG_INT: *this = detail::decode<TagInt>(is); break;
+        case TAG_LONG: *this = detail::decode<TagLong>(is); break;
+        case TAG_FLOAT: *this = detail::decode<TagFloat>(is); break;
+        case TAG_DOUBLE: *this = detail::decode<TagDouble>(is); break;
+        case TAG_BYTE_ARRAY: *this = detail::decode<TagByteArray>(is); break;
+        case TAG_STRING: *this = detail::decode<TagString>(is); break;
+        case TAG_LIST: *this = detail::decode<TagList>(is); break;
+        case TAG_COMPOUND: *this = detail::decode<TagCompound>(is); break;
+        case TAG_INT_ARRAY: *this = detail::decode<TagIntArray>(is); break;
+        case TAG_LONG_ARRAY: *this = detail::decode<TagLongArray>(is); break;
+        default: throw std::runtime_error {"invalid tag type"};
+    }
+}
+
+NBT::NBT(std::istream& is)
+{
+    this->decode(is);
+}
+
+NBT::NBT(std::istream&& is)
+{
+    this->decode(is);
+}
+
+void NBT::encode(std::ostream& os) const
+{
+    if (this->data)
+    {
+        if (const auto& tags = std::get_if<TagCompound>(&this->data->tags))
+        {
+            detail::encode<TagByte>(os, TAG_COMPOUND);
+            detail::encode(os, this->data->name);
+            detail::encode(os, *tags);
+        }
+        else if (const auto& tags = std::get_if<TagList>(&this->data->tags))
+        {
+            detail::encode<TagByte>(os, TAG_LIST);
+            detail::encode(os, this->data->name);
+            detail::encode(os, *tags);
+        }
+        else
+            throw std::runtime_error {"invalid tag type"};
+    }
+    else
+        detail::encode<TagEnd>(os);
+}
+
+void NBT::encode(std::ostream&& os) const
+{
+    this->encode(os);
+}
+
+void NBT::decode(std::istream& is)
+{
+    const auto type = detail::decode<TagByte>(is);
+
+    if (type == TAG_COMPOUND)
+        this->data = NBTData {detail::decode<TagString>(is), detail::decode<TagCompound>(is)};
+    else if (type == TAG_LIST)
+        this->data = NBTData {detail::decode<TagString>(is), detail::decode<TagList>(is)};
+    else
+        throw std::runtime_error {"invalid tag type"};
+}
+
+void NBT::decode(std::istream&& is)
+{
+    this->decode(is);
+}
+
+const TagString& NBT::name() const
+{
+    return this->data->name;
+}
+
+template<typename T>
+Tag& NBT::operator[](int index) requires detail::Is_tag_type<T>
+{
+    return std::get<std::vector<T>>(std::get<TagList>(this->data->tags))[index];
+}
+
+Tag& NBT::operator[](const TagString& key)
+{
+    return std::get<TagCompound>(this->data->tags)[key];
+}
+
+}  // namespace nbt
---
+++ nbt.h
@@ -0,0 +1,184 @@
+/**
+ * Author Root (Discord: stdio.h#5797; GitHub: 0x526f6f74)
+ **/
+
+#pragma once
+
+#include <istream>
+#include <map>
+#include <optional>
+#include <ostream>
+#include <string>
+#include <variant>
+#include <vector>
+
+namespace nbt
+{
+
+enum TagType
+{
+    TAG_END,
+    TAG_BYTE,
+    TAG_SHORT,
+    TAG_INT,
+    TAG_LONG,
+    TAG_FLOAT,
+    TAG_DOUBLE,
+    TAG_BYTE_ARRAY,
+    TAG_STRING,
+    TAG_LIST,
+    TAG_COMPOUND,
+    TAG_INT_ARRAY,
+    TAG_LONG_ARRAY
+};
+
+using TagEnd = std::nullptr_t;
+
+using TagByte = std::int8_t;
+using TagShort = std::int16_t;
+using TagInt = std::int32_t;
+using TagLong = std::int64_t;
+
+using TagFloat = float;
+using TagDouble = double;
+
+using TagString = std::string;
+
+using TagByteArray = std::vector<TagByte>;
+using TagIntArray = std::vector<TagInt>;
+using TagLongArray = std::vector<TagLong>;
+
+class Tag;
+class TagList;
+
+using TagCompound = std::map<TagString, Tag>;
+
+namespace detail
+{
+
+template<typename T>
+concept Is_tag_end = std::same_as<T, TagEnd> || std::same_as<T, TagEnd&>;
+
+template<typename T>
+concept Is_tag_numeric = std::integral<T> || std::floating_point<T>;
+
+template<typename T>
+concept Is_tag_string = std::same_as<T, TagString>;
+
+template<typename T>
+concept Is_tag_array = std::same_as<T, TagByteArray> || std::same_as<T, TagIntArray> || std::same_as<T, TagLongArray>;
+
+template<typename T>
+concept Is_tag_list = std::same_as<T, TagList>;
+
+template<typename T>
+concept Is_tag_compound = std::same_as<T, TagCompound>;
+
+template<typename T>
+concept Is_tag_or_tag_list = std::same_as<T, Tag> || Is_tag_list<T>;
+
+template<typename T>
+concept Is_tag_type = Is_tag_end<T> || Is_tag_numeric<T> || Is_tag_string<T> || Is_tag_array<T> || Is_tag_list<T> || Is_tag_compound<T>;
+
+void encode(std::ostream& os, Is_tag_end auto = nullptr);
+
+void encode(std::ostream& os, Is_tag_numeric auto value);
+
+void encode(std::ostream& os, const Is_tag_string auto& string);
+
+void encode(std::ostream& os, const Is_tag_array auto& vector);
+
+void encode(std::ostream& os, const Is_tag_or_tag_list auto& value);
+
+void encode(std::ostream& os, const Is_tag_compound auto& compound);
+
+template<Is_tag_end T>
+T decode(std::istream& is);
+
+template<Is_tag_numeric T>
+T decode(std::istream& is);
+
+template<Is_tag_string T>
+T decode(std::istream& is);
+
+template<Is_tag_array T>
+T decode(std::istream& is);
+
+template<Is_tag_list T>
+T decode(std::istream& is);
+
+template<Is_tag_compound T>
+T decode(std::istream& is);
+
+}  // namespace detail
+
+class TagList : public std::variant<
+                    std::vector<TagEnd>,
+                    std::vector<TagByte>,
+                    std::vector<TagShort>,
+                    std::vector<TagInt>,
+                    std::vector<TagLong>,
+                    std::vector<TagFloat>,
+                    std::vector<TagDouble>,
+                    std::vector<TagByteArray>,
+                    std::vector<TagString>,
+                    std::vector<TagList>,
+                    std::vector<TagCompound>,
+                    std::vector<TagIntArray>,
+                    std::vector<TagLongArray>>
+{
+public:
+    using variant::variant;
+
+    TagList(std::istream& is);
+
+    void encode(std::ostream& os) const;
+    void decode(std::istream& is);
+
+private:
+    template<typename T>
+    std::vector<T> decode_vector(std::istream& is) requires detail::Is_tag_type<T>;
+};
+
+class Tag
+  : public std::
+        variant<TagEnd, TagByte, TagShort, TagInt, TagLong, TagFloat, TagDouble, TagByteArray, TagString, TagList, TagCompound, TagIntArray, TagLongArray>
+{
+public:
+    using variant::variant;
+
+    Tag(std::istream& is, TagType type);
+
+    void encode(std::ostream& os) const;
+    void decode(std::istream& is, TagType type);
+};
+
+struct NBTData
+{
+    TagString name;
+    std::variant<TagList, TagCompound> tags;
+};
+
+class NBT
+{
+public:
+    NBT(std::istream& is);
+    NBT(std::istream&& is);
+
+    void encode(std::ostream& os) const;
+    void encode(std::ostream&& os) const;
+    void decode(std::istream& is);
+    void decode(std::istream&& is);
+
+    const TagString& name() const;
+
+    template<typename T>
+    Tag& operator[](int index) requires detail::Is_tag_type<T>;
+
+    Tag& operator[](const TagString& key);
+
+private:
+    std::optional<NBTData> data;
+};
+
+}  // namespace nbt
